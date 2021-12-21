c8b6dcf8ea9bbb1e871b798f5a1d5c53a696ac60
Max <email>
2021-12-21 17:56:43 +1300

    fixed Phase_matching order error

--- optic_sim_pack/AuxFuncs/Phase_matching.py
+++ optic_sim_pack/AuxFuncs/Phase_matching.py
@@ -12,7 +12,8 @@ def delta_phi_calc(params):
     cw_min, cw_max = cw_return(params.del0, params.alpha, params.P_in, params.gamma, params.L, params.theta1)
 
     dphi = np.zeros(len(f_sample))
-    for n in range(2, params.order + 1):
+    for n in betak.keys():
+        if n > params.order: break
         dphi += betak[n] * params.L * f_sample**n/factorial(n)
     dphi += 2 * cw_max * params.gamma* params.L - params.del0 - params.d * f_sample
 
@@ -23,7 +24,8 @@ def delta_g_calc(params):
     f_sample = params.f_plot * 2 * np.pi
 
     dphi = np.zeros(len(f_sample))
-    for n in range(2, params.order + 1):
+    for n in betak.keys():
+        if n > params.order: break
         dphi += betak[n] * params.L * f_sample**(n-1)/factorial(n-1)
     dphi -= params.d * params.L
     return dphi
